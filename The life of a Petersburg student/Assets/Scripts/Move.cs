d4e8b2a44512b5cbbe6f0af03b55046a0991835e
DebtOfTime <sasha_kulagin098311@mail.ru>
2025-04-11 15:04:13 +0300

    Move

---
+++ The life of a Petersburg student/Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //float directionX = Input.GetAxis("Horizontal");
+        //float directionY = Input.GetAxis("Vertical");
+        //transform.position += new Vector3(Time.deltaTime*moveSpeed, 0);
+        if (direction.x != 0 && direction.y != 0)
+        {
+            direction.Normalize();
+        }
+        rb.linearVelocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+    }
+}
